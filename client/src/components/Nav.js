d58135c835f48e9e60b5b7d82639a78a15ec952c
christianhoward <christian.john.howard@gmail.com>
2018-05-27 17:27:02 -0400

    Refactored Nav into a stateless component.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Nav extends Component {
-    render() {
-        return (
-            <div className="nav">
-                <div className="nav-logo">
-                    <Link to="/">Contact List App</Link>
-                </div>
-                <div className="nav-links">
-                    <Link className="nav-link" to="/form"><i className="fas fa-plus-circle nav-item__icon"></i></Link>
-                </div>
-            </div>
-        );
-    }
-}
-
+const Nav = () => (
+    <div className="nav">
+        <div className="nav-logo">
+            <Link to="/">Contact List App</Link>
+        </div>
+        <div className="nav-links">
+            <Link className="nav-link" to="/form"><i className="fas fa-plus-circle nav-item__icon"></i></Link>
+        </div>
+    </div>
+);
 export default Nav;
