c4671dcaaa810e7afaadaf476d7e0274c8621a41
christianhoward <christian.john.howard@gmail.com>
2018-05-28 01:57:23 -0400

    Changed Phone Number RegExp to handle most forms of phone number entry.

--- client/src/components/formFields.js
+++ client/src/components/formFields.js
@@ -2,6 +2,6 @@ export default [
     { label: 'First Name', name: 'firstname', type: 'text', placeholder: 'Enter First Name', pattern: '[a-zA-Z]+' }, 
     { label: 'Last Name', name: 'lastname', type: 'text', placeholder: 'Enter Last Name', pattern: '[a-zA-Z]+' }, 
     { label: 'E-Mail', name: 'email', type: 'email', placeholder: 'Enter E-Mail Address', pattern: '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$' }, 
-    { label: 'Phone Number', name: 'phone', type: 'tel', placeholder: 'XXX-XXX-XXXX', pattern: '^\\d{3}-\\d{3}-\\d{4}$' }, 
+    { label: 'Phone Number', name: 'phone', type: 'tel', placeholder: 'XXX-XXX-XXXX', pattern: '^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}$' }, 
     { label: 'Status', name: 'status', type: 'text', placeholder: 'Enter Status' }, 
 ]
